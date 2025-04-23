9c96b9f9d3d39a00a19e3763d0d0200b5ba5b6bf
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-04-23 14:30:59 -0300

    Wrap application in QueryClientProvider to enable React Query functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 import Error from './pages/Error';
+import Home from './pages/Home';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path="*" element={<Error />} />
-			</Routes>
-		</BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
 	</StrictMode>
 );
