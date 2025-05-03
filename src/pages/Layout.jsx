a7b66396f7fca9e4ada42970ce1b61a47c3ea446
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-05-03 10:48:10 -0300

    Add layout component with header, footer, and navigation styles

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import styles from './stylesheets/Layout.module.css';
 
 const Layout = () => {
 	return (
 		<div>
 			<header>
+				<img
+					className={styles.logo}
+					src="/preçoRadar-LOGO.png"
+					alt="Logotipo do site que mostra um carrinho de supermercado com um sinal sendo emitido dele."
+				/>
 				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/average-prices">Médias Diárias</Link>
-					</li>
-					<li>
-						<Link to="/prices-per-market">Preços por mercado</Link>
-					</li>
-					<li>
-						<Link to="/prices-per-brand">Preços por marca</Link>
-					</li>
-					<li>
-						<Link to="/latest-prices">Preços mais recentes</Link>
-					</li>
+					<div className={styles.navbar}>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/average-prices">Médias Diárias</Link>
+						</li>
+						<li>
+							<Link to="/prices-per-market">Preços por mercado</Link>
+						</li>
+						<li>
+							<Link to="/prices-per-brand">Preços por marca</Link>
+						</li>
+						<li>
+							<Link to="/latest-prices">Preços mais recentes</Link>
+						</li>
+					</div>
 				</ul>
 			</header>
 			<main>
