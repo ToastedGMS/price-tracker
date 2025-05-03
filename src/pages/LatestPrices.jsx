a822450d6a7689f5e936940be12462e2bac9daa4
ToastedGMS <gabrielsiqueira3052@gmail.com>
2025-05-03 11:23:10 -0300

    Add container styling to LatestPrices component for improved layout

--- src/pages/LatestPrices.jsx
+++ src/pages/LatestPrices.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import LatestTable from '../reusable/LatestTable';
+import styles from './stylesheets/LatestPrices.module.css';
 
 const LatestPrices = () => {
 	return (
-		<div>
+		<div className={styles.container}>
 			<LatestTable type="cafe" />
 			<LatestTable type="arroz" />
 			<LatestTable type="feijao" />
